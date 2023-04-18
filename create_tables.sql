d8260ddca948f461f26e897cb64a5a16f836aabe
Filip Casey <filip.r.casey@gmail.com>
2023-04-18 14:45:22 -0600

    docker updates

--- create_tables.sql
+++ create_tables.sql
@@ -1,12 +1,17 @@
 CREATE TABLE IF NOT EXISTS machine_links(
     ccn INT NOT NULL,
     state_or_region varchar(50) NOT NULL, 
-    name varchar(200) NOT NULL, 
+    name varchar(200) NOT NULL,
+    machine_readable_url varchar(500),
     PRIMARY KEY (ccn)
 );
 
 CREATE TABLE IF NOT EXISTS features(
     bed_count INT NOT NULL,
     zipcode varchar(10),
-    medicare BOOLEAN NOT NULL
+    medicare BOOLEAN NOT NULL,
+    ccn INT NOT NULL,
+    CONSTRAINT fk_ccn
+        FOREIGN KEY(ccn)
+            REFERENCES machine_links(ccn)
 );
